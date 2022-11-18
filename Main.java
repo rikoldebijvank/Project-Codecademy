801ebcd37527076f5588552cd79beedd05b9e181
BBayrkdar <b.bayrkdar@student.avans.nl>
2022-11-18 23:25:20 +0100

    Baraa's commit

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Rik's Commit");
+
+        System.out.println("Baraa's Comit");
     
         System.out.println("Mert's Commit");
 
