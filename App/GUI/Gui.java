62d7e72d2d5a5d279e5ba97275765b4568554071
BBayrkdar <b.bayrkdar@student.avans.nl>
2022-12-14 18:35:07 +0100

    test

--- App/GUI/Gui.java
+++ App/GUI/Gui.java
@@ -1,7 +1,14 @@
 package App.GUI;
 
-public class Gui {
-    public static void start(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Gui extends Application {
+    public static void start(Stage stage) throws Exception {
+
+        Scene mainview = new Scene(totalPane);
+        stage.setScene(mainview);
+        stage.show();
     }
 }
