243d24d921054757eff72654c8e283d624489f01
BBayrkdar <b.bayrkdar@student.avans.nl>
2022-12-15 09:58:06 +0100

    name

--- App/Main.java
+++ App/Main.java
@@ -11,11 +11,8 @@ public class Main {
     public static void main(String[] args) {
 
         Application.launch(Gui.class);
-<<<<<<< HEAD
 
         String connectionUrl = "jdbc:sqlserver://localhost;databaseName=Codecademy;integratedSecurity=true;encrypt=true;trustServerCertificate=true;";
-=======
->>>>>>> 41a2c87b756bd990c2aa6b63fb10a0a34a3a3dfd
 
         // DatabaseController control = new DatabaseController();
         // String signInSQL = "SELECT * FROM Users";
