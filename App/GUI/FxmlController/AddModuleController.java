a9d3f59bbd42b88e9b743ca00442bf4595b46c86
BBayrkdar <b.bayrkdar@student.avans.nl>
2023-01-15 19:08:19 +0100

    beschikbare cursussen pagina

---
+++ App/GUI/FxmlController/AddModuleController.java
@@ -0,0 +1,12 @@
+package App.GUI.FxmlController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import App.DatabaseController;
+import javafx.fxml.Initializable;
+
+public class AddModuleController extends AddCourseController{
+
+    private DatabaseController controller = new DatabaseController();
+    
+}
