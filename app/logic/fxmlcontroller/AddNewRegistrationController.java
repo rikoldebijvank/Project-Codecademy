8fdce6176bdbaecb2642a3b11a4612d0b70c0abe
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-04-16 13:41:02 +0200

    Adding new Registration debugged

--- app/logic/fxmlcontroller/AddNewRegistrationController.java
+++ app/logic/fxmlcontroller/AddNewRegistrationController.java
@@ -6,9 +6,10 @@ import app.database.DatabaseController;
 import app.presentation.Gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class AddNewRegistrationController {
+public class AddNewRegistrationController extends Controller {
     
     DatabaseController controller = new DatabaseController();
     Gui gui = new Gui();
@@ -19,9 +20,17 @@ public class AddNewRegistrationController {
     private TextField studentEmailTextField;
     @FXML
     private Button addNewRegistrationBtn;
+    @FXML
+    private Label addingNewRegLabel;
+    
     private LocalDate currentDate;
 
     public void addNewRegistration() throws Exception{
+
+        if (courseNameTextField.getText().isEmpty() || studentEmailTextField.getText().isEmpty()) {
+            addingNewRegLabel.setText("Fields shouldn't be empty");
+            return;
+        }
         String courseName = courseNameTextField.getText().toString();
         String studentEmail = studentEmailTextField.getText().toString();
         currentDate = LocalDate.now();
